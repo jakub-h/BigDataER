ce94b8e45536146fc3c7cc488f2eca93f57ae93f
mariastr <mariastr@luo065lap049.staff.ad.uta.fi>
2019-02-24 18:11:47 +0200

    bugs ... bugs ... BUUUUGGGSSSSSSSS

---
+++ src/Blocking/BlockCollection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Blocking;
+
+import java.util.List;
+
+/**
+ *
+ * @author mariastr
+ */
+public class BlockCollection {
+    List<Block> list;
+}
