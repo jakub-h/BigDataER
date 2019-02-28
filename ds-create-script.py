5696203baff18aa62d5c0b58795df6a8d3a75724
Jakub Hruška <jhruska@mail.muni.cz>
2019-03-01 01:21:20 +0200

    source dataset created

--- ds-create-script.py
+++ ds-create-script.py
@@ -36,10 +36,64 @@ def filter_professions():
     directors.to_csv("datasets/tmp/directors.csv", index=False, sep="\t")
 
 
+def filter_num_of_titles():
+    dsets = ["directors", "actresses", "actors"]
+    for dataset in dsets:
+        people = pd.read_csv("datasets/tmp/{}.csv".format(dataset), delimiter="\t")
+        filtered = []
+        for index, person in people.iterrows():
+            if len(person["knownForTitles"].split(",")) > 1:
+                filtered.append(person)
+        result = pd.DataFrame(filtered)
+        result.to_csv("datasets/tmp/{}_filtered.csv".format(dataset), index=False, sep="\t")
+
+
+def filter_films():
+    films = pd.read_csv("datasets/original/title.basic.tsv", delimiter="\t")
+    films = films.dropna(how="any")
+    films = films.drop(["titleType", "originalTitle", "isAdult", "endYear", "runtimeMinutes", "genres"], axis=1)
+    films.to_csv("datasets/tmp/films_filtered.csv", index=False, sep="\t")
+
+
+def create_source_dataset():
+    directors = pd.read_csv("datasets/tmp/directors_filtered.csv", delimiter="\t")
+    films = pd.read_csv("datasets/tmp/films_filtered.csv", delimiter="\t")
+    actors = pd.read_csv("datasets/tmp/actors_filtered.csv", delimiter="\t")
+    actresses = pd.read_csv("datasets/tmp/actresses_filtered.csv", delimiter="\t")
+    with open("datasets/final/source.csv", 'a') as output:
+        names = ["Sergio Leone", "Akira Kurosawa", "Steven Spielberg", "George Lucas"]  # and so on...
+        for name in names:
+            for i_dir, director in directors.loc[directors['primaryName'] == name].iterrows():
+                print(director)
+                found_films = films.loc[films['tconst'].isin(
+                    directors.loc[directors['primaryName'] == name].iloc[0]["knownForTitles"].split(","))]
+                for i_film, film in found_films.iterrows():
+                    output.write(film["primaryTitle"] + ";")
+                    output.write(director["primaryName"] + ";")
+                    output.write(film["startYear"] + ";")
+                    for i_actor, actor in actors.iterrows():
+                        if film['tconst'] in actor["knownForTitles"].split(","):
+                            output.write(actor["primaryName"] + ";")
+                            break
+                    for i_actress, actress in actresses.iterrows():
+                        if film['tconst'] in actress["knownForTitles"].split(","):
+                            output.write(actress["primaryName"])
+                            break
+                    output.write("\n")
+
+
 if __name__ == '__main__':
     # split_data()
     # filter_professions()
-    pass
+    # filter_num_of_titles()
+    # filter_films()
+    create_source_dataset()
+
+
+
+
+
+
 
 
 
