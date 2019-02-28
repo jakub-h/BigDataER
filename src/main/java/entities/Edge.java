23f76c5639b6d90e6b10ca693df0cba60dd07ef5
mariaStratigi <47947413+mariaStratigi@users.noreply.github.com>
2019-02-28 17:22:57 +0200

    Add files via upload
    Metablocking is done

--- src/main/java/entities/Edge.java
+++ src/main/java/entities/Edge.java
@@ -1,4 +1,48 @@
-package entities;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graph;
 
+/**
+ *
+ * @author mariastr
+ */
 public class Edge {
+    private String id;
+    private int commonBlocks = 1;
+    private double jaccard;
+
+    
+    public Edge(String id){
+        this.id = id;
+    }
+    
+    
+    public String getId() {
+        return id;
+    }
+
+    public int getCommonBlocks() {
+        return commonBlocks;
+    }
+
+    public double getJaccard() {
+        return jaccard;
+    }
+    
+    
+    public String toString(){
+        String s = id;
+        return s;
+    }
+    
+    public void addCommonBlock(){
+        commonBlocks++;
+    }
+
+    public void setJaccard(double jaccard) {
+        this.jaccard = jaccard;
+    }
 }
--- src/main/java/entities/Node.java
+++ src/main/java/entities/Node.java
@@ -1,4 +1,75 @@
-package entities;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author mariastr
+ */
 public class Node {
+    
+    private Long id;
+    List<Edge> edges;
+    
+    
+    public Node(Long id){
+        this.id = id;
+        edges = new ArrayList<>();
+    }
+    
+    
+    public void addEdge(Edge e){
+        edges.add(e);
+    }
+    
+    public String toString(){
+        String s = "";
+        s = s + id + "[";
+        for(Edge e : edges){
+            s = s + e.toString() + " ";
+        }
+        s = s + "]";
+        return s;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+    
+    public void removeEdge(String s){
+        int i = 0;
+        for(Edge e : edges){
+            if(s.equalsIgnoreCase(e.getId())){
+                break;
+            }
+            i++;
+        }
+        if(i < edges.size()){
+            edges.remove(i);
+        }
+    }
+    
+    public void removeEdges(List<String> keep){
+        List<Edge> rem = new ArrayList<>();
+        
+        for(Edge e : edges){
+            if(!keep.contains(e.getId())){
+                rem.add(e);
+            }
+        }
+        
+        edges.removeAll(rem);
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    
 }
